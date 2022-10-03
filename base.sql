928af6d83c28abc382b8a7e5fc26698a7afc646c
herilion <heritierlionge@yahoo.com>
2022-10-03 15:06:03 +0200

    ajout de plusieurs elements à la fois

--- base.sql
+++ base.sql
@@ -1,2 +1,10 @@
 INSERT INTO utilisateur(nom,prenom,email)
-VALUES ('Lionge','Heritier','heritierlionge@yahoo.com')
+-- Ajout d'un élément
+VALUES ('Lionge','Heritier','heritierlionge@yahoo.com');
+
+--Ajout de plusieurs éléments
+INSERT INTO utilisateur(nom,prenom,email)
+VALUES 
+('Lionge','Heritier','heritierlionge@yahoo.com'),
+('Bushiri','Bonheur','bonheurbish@gmail.com'),
+('Mongendo','Christian','chrisheritierlionge@gmail.com');
