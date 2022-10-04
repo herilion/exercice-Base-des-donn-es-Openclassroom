44f252174fa6864a469ee455b667db6f563c356c
herilion <heritierlionge@yahoo.com>
2022-10-04 15:11:44 +0200

    creéation de la table utilisateur

--- base.sql
+++ base.sql
@@ -1,4 +1,10 @@
 -- création d'une table
+CREATE TABLE utilisateur(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(150) NOT NULL DEFAULT 'PAS DE NOM',
+    prenom VARCHAR(150) NOT NULL DEFAULT 'PAS DE PRENOM'
+    email VARCHAR(200) NOT NULL UNIQUE
+);
 INSERT INTO utilisateur(nom,prenom,email)
 -- Ajout d'un élément
 VALUES ('Lionge','Heritier','heritierlionge@yahoo.com');
